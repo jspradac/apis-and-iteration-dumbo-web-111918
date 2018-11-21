8097c1e79da1630349f4dd5b0bcfe77e43b12f1c
jspradac <jspradac@gmail.com>
2018-11-20 23:00:55 -0500

    finished this project with the bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,11 +1,13 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-
-def get_character_movies_from_api(character_name)
-  #make the web request
+def find_character(character)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  response_hash["results"].find {|character_hash| (character_hash.values).include?(character)}
+end
+def get_character_movies_from_api(character)
+  #make the web request
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -15,11 +17,15 @@ def get_character_movies_from_api(character_name)
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  #  of movies by title. Have a play around with the puts with other info about a given films
+  find_character(character)["films"].collect {|film| JSON.parse(RestClient.get(film))}
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    puts "- " + film["title"]
+  end
 end
 
 def show_character_movies(character)
@@ -27,6 +33,14 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+def get_films_from_api(film_title)
+  #make the web request
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
+# films = get_character_movies_from_api("Luke Skywalker")
+# print_movies(films)
+
+end
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
